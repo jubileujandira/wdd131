f6128714d98aa1ec6bad40d6c40e12041da00ba9
leandromoura <jmoura3@byupathway.edu>
2026-07-04 17:40:37 -0300

    bash

---
+++ scripts/obterdatas.js
@@ -0,0 +1,4 @@
+document.getElementById("anoatual").textContent = new Date().getFullYear();
+
+document.getElementById("ultimaModificacao").textContent =
+"Última modificação: " + document.lastModified;
